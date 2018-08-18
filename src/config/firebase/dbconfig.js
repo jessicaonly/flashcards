ec7c4447d86ef39f7c1782a7490b1661e9890dee
Jessica Skok <jessica.skok@gmail.com>
2018-08-18 01:22:30 -0700

    flashcard application with firebase

---
+++ src/config/firebase/dbconfig.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyDSbSEP7sMuT-9wYfFF08rnO4dcgOaDDi4",
+  authDomain: "react-flashcards-de21e.firebaseapp.com",
+  databaseURL: "https://react-flashcards-de21e.firebaseio.com",
+  projectId: "react-flashcards-de21e",
+  storageBucket: "react-flashcards-de21e.appspot.com",
+  messagingSenderId: "410285737634"
+}
