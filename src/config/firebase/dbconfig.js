4b5bc1d5d15402db6e235010028788a2a476c815
jessicaonly <jessica.skok@gmail.com>
2018-08-18 01:29:41 -0700

    Delete dbconfig.js

--- src/config/firebase/dbconfig.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-  apiKey: "AIzaSyDSbSEP7sMuT-9wYfFF08rnO4dcgOaDDi4",
-  authDomain: "react-flashcards-de21e.firebaseapp.com",
-  databaseURL: "https://react-flashcards-de21e.firebaseio.com",
-  projectId: "react-flashcards-de21e",
-  storageBucket: "react-flashcards-de21e.appspot.com",
-  messagingSenderId: "410285737634"
-}
